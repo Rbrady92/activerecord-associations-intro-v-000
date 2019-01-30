dc58f503ad0e28df01203c012abbad325a4f683f
Ryan Brady <ryanbrady369@gmail.com>
2019-01-30 21:57:34 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,8 +1,8 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
-  def change 
+  def change
     create_table :songs do |q|
-      q.string :name 
+      q.string :name
       q.integer :artist_id
       q.integer :genre_id
-    end 
+    end
 end
